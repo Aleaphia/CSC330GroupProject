a4afb34df707108949522b8f71a2a42fc8983c5e
FMohamud23 <Farhan.mohamud46@gmail.com>
2025-04-23 21:53:27 -0500

    Updated BankAccount.py

--- Bank/BankAccount.py
+++ Bank/BankAccount.py
@@ -1,26 +1,29 @@
 # BankAccount.py
-# WRITE SHORT DESCRIPTION
+# The BankAccount class is a program that simulates basic banking. It lets you create an account with personal information, 
+# an account number, and a balance. You can deposit or withdraw money from the account.The account number must follow a specific format, 
+# starting with the person's first and last name initials and followed by 6 digits.
 #
 # Created for CSC 330: Language Design and Implementation
 # With Professor Dawn Duerre
 #
 # Created 4/13/25 by Shoshana Altman
-# Updated....
+# Updated: Farhan Mohamud 4/24/2025
 #
 # I certify that this computer program is all my own work.
-# Signed:
+# Signed: Farhan Mohamud
+
 
 ###################################
-####### BANK ACCOUNT CLASS ########
+# BANK ACCOUNT CLASS ########
 ###################################
 class BankAccount:
-    """BankAccount class to store account details and perform transactions."""
-    
     # Constructor to initialize the bank account with personal details and balance
     def __init__(self, first_name, last_name, account_number, balance):
+        # Private attributes
         self.__first_name = first_name
         self.__last_name = last_name
-        self.__account_number = account_number
+        self.__account_number = None
+        self.set_account_number(account_number)
         self.__balance = balance
 
     # Getters for account details
@@ -36,9 +39,31 @@ class BankAccount:
     def get_balance(self):
         return self.__balance
 
-    # Setter to update the balance
-    def set_balance(self, amount):
-        self.__balance = amount
+    # Setters to update private attributes
+    def set_first_name(self, first_name):
+        self.__first_name = first_name
+
+    def set_last_name(self, last_name):
+        self.__last_name = last_name
+
+    def set_account_number(self, account_number):
+        if self.is_valid_account_number(account_number):
+            self.__account_number = account_number
+        else:
+            raise ValueError(f"Invalid account number format: {account_number}. Must start with initials and be 8 digits.")
+
+    def set_balance(self, balance):
+        self.__balance = balance
+
+    # Method to validate the account number
+    def is_valid_account_number(self, account_number):
+        if len(account_number) != 8:
+            return False
+        if not account_number[:2].isalpha() or not account_number[2:].isdigit():
+            return False
+        if account_number[:2].upper() != (self.__first_name[0] + self.__last_name[0]).upper():
+            return False
+        return True
 
     # Method to deposit an amount into the account
     def deposit(self, amount):
